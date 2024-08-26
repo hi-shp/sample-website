e15a968441dc5c6c1591a14b8a96a013fa087eaf
Soonhong Park <168538598+hi-shp@users.noreply.github.com>
2024-08-27 00:53:54 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,48 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Nav from './component/Nav';
-import Intro from './component/Intro';
-import Function from './component/Function';
-import Price from './component/Price';
-import Footer from './component/Footer';
+import React, { useState } from "react";
+import './App.css';
+import { Nav } from './component/Nav';
+import { Intro } from './component/Intro';
+import { Function } from './component/Function';
+import Price  from './component/Price';
+import { Footer } from './component/Footer';
 import Feedback from './component/Feedback';
+import Modal from "./component/Modal";
+
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
   return (
-    <Router>
-      <div className="App">
-        <header>
-          <Nav />
-        </header>
-        <main>
-          <Routes>
-            <Route path="/" element={<Intro />} />
-            <Route path="/features" element={<Function />} />
-            <Route path="/price" element={<Price />} />
-            <Route path="/feedback" element={<Feedback />} />
-          </Routes>
-        </main>
-        <footer>
-          <Footer />
-        </footer>
+    <div className="App">
+
+      <div>
+        {/* Modal 컴포넌트를 표시, isOpen 상태로 열고 닫기 제어 */}
+        <Modal isOpen={isModalOpen} onClose={closeModal} />
       </div>
-    </Router>
+
+      <header>
+        <Nav className="navbar" />
+      </header>
+
+      <main>
+        <Intro className="intro" />
+        <Function className="function"/>
+        <Price className="price"/>
+        <Feedback className="feedback"/>
+      </main>
+
+      <footer>
+        <Footer className="footer"/>
+      </footer>
+    </div>
   );
 }
 
